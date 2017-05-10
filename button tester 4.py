e4f244dfe7e77531ecfa7e41d2b0a7ec04b5ea5e
Kew4sK <pmace088@gmail.com>
2017-05-10 16:07:40 -0500

    Update button tester 4.py

--- button tester 4.py
+++ button tester 4.py
@@ -10,9 +10,8 @@ import RPi.GPIO as GPIO
 from time import sleep
 import pygame
 
-#Set switches and leds to pins from left to right
-switches = [18, 23, 24, 25, 12, 16, 20, 21, 19, 26]
-leds = [17, 22, 5, 6, 13]
+#Set switches from left to right
+switches = [26, 19, 21, 20, 16, 12, 25, 24, 23, 18]
 
 #set 
 GPIO.setmode(GPIO.BCM)
@@ -34,32 +33,32 @@ h = False
 try:    
     while (True):
         #sets each button as an input state
-        input_state0 = GPIO.input(18)
-        input_state1 = GPIO.input(23)
-        input_state2 = GPIO.input(24)
-        input_state3 = GPIO.input(25)
-        input_state4 = GPIO.input(12)
-        input_state5 = GPIO.input(16)
-        input_state6 = GPIO.input(20)
-        input_state7 = GPIO.input(21)
-        input_state8 = GPIO.input(19)
-        input_state9 = GPIO.input(26)
+        input_state0 = GPIO.input(26)
+        input_state1 = GPIO.input(19)
+        input_state2 = GPIO.input(21)
+        input_state3 = GPIO.input(20)
+        input_state4 = GPIO.input(16)
+        input_state5 = GPIO.input(12)
+        input_state6 = GPIO.input(25)
+        input_state7 = GPIO.input(24)
+        input_state8 = GPIO.input(23)
+        input_state9 = GPIO.input(18)
         #if the button is pressed the variable becomes true
-        if (input_state0 | a):
+        if (input_state9 | a):
             a = True
-            if (input_state1 | b):
+            if (input_state0 | b):
                 b = True
-                if (input_state2 | c):
+                if (input_state8 | c):
                     c = True
-                    if (input_state3 | d):
+                    if (input_state2 | d):
                         d = True
-                        if (input_state4 | e):
+                        if (input_state1 | e):
                             e = True
-                            if (input_state5 | f):
+                            if (input_state7 | f):
                                 f = True
-                                if (input_state6 | g):
+                                if (input_state4 | g):
                                     g = True
-                                    if (input_state7 | h):
+                                    if (input_state3 | h):
                                         h = True
                                         if (h == True):
                                             print "Succesful Code Entry!"
